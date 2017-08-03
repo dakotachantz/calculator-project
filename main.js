072d1c254e78f9fa64da18ce83f2335fadc281b7
Dakota Nichols <dakotachantz@gmail.com>
2017-08-03 15:47:16 -0500

    all functions work

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-let button = document.querySelector(".button");
-let display = document.querySelector("div.display");
-let displayContain = document.querySelector("#display");
-// let text = document.createElement("p");
-// button.addEventListener();
+
+let display = document.querySelector(".display");
+let clear = document.querySelector("#clear");
+let buttons = document.querySelectorAll(".button");
+let equals = document.querySelector("#keyEqual");
+let squareRt = document.querySelector("#keySqRt");
+let output = [];
+
+
+squareRt.addEventListener("click", function (e) {
+    display.innerHTML = Math.sqrt(display.innerHTML);
+    output.push(display.innerHTML);
+});
+equals.addEventListener("click", function (e) {
+    // console.log(e);
+    display.innerHTML = eval(display.innerHTML);
+    output.push(display.innerHTML);
+});
+
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function (e) {
+
+        display.innerHTML += e.target.innerHTML;
+    });
+
+} //end of for loop
+
+clear.addEventListener("click", function () {
+    display.innerHTML = "";
+});
 
 
 
-display.innerHTML = "1";
-console.log(display.innerHTML);
-display.innerHTML += "+7";
-console.log(display.innerHTML);
-console.log(+display.innerHTML);
+// console.log(buttons);
 
-// display.appendChild(text);
-
-// displayContain.appendChild(display);
 
 
 
