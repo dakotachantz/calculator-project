204c09cf3d24b2d5073af558809b1f2e4095e0b5
Dakota Nichols <dakotachantz@gmail.com>
2017-08-03 13:23:47 -0500

    changed styling back to divs

---
+++ main.js
@@ -0,0 +1,20 @@
+let button = document.querySelector(".button");
+let display = document.querySelector("div.display");
+let displayContain = document.querySelector("#display");
+// let text = document.createElement("p");
+// button.addEventListener();
+
+
+
+display.innerHTML = "1";
+console.log(display.innerHTML);
+display.innerHTML += "+7";
+console.log(display.innerHTML);
+console.log(+display.innerHTML);
+
+// display.appendChild(text);
+
+// displayContain.appendChild(display);
+
+
+
