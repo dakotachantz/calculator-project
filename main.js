5aa4baed9568361c76ec430e5da43f16b85300c6
Dakota Nichols <dakotachantz@gmail.com>
2017-08-03 18:00:05 -0500

    scroll up through previous operations works until end of array

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ let buttons = document.querySelectorAll(".button");
 let equals = document.querySelector("#keyEqual");
 let squareRt = document.querySelector("#keySqRt");
 let output = [];
-
+let body = document.querySelector("body");
+let outputIndex;
 
 squareRt.addEventListener("click", function (e) {
     display.innerHTML = Math.sqrt(display.innerHTML);
     output.push(display.innerHTML);
+    outputIndex = output.length - 1;
+    console.log(output);
 });
+
 equals.addEventListener("click", function (e) {
-    // console.log(e);
     display.innerHTML = eval(display.innerHTML);
     output.push(display.innerHTML);
+    outputIndex = output.length - 1;
+    console.log(output);
 });
 
 for (let i = 0; i < buttons.length; i++) {
@@ -32,7 +37,22 @@ clear.addEventListener("click", function () {
 
 
 
-// console.log(buttons);
+body.addEventListener("keydown", function (e) {
+    if (e.keyCode === 38) {
+        if (outputIndex < 0) {
+            outputIndex = output[0];
+        }
+        display.innerHTML = output[outputIndex];
+        outputIndex--;
+    } else if (e.keyCode === 40) {
+        if (outputIndex >= output.length) {
+            outputIndex = output.length - 1;
+        }
+        display.innerHTML = output[outputIndex];
+        outputIndex++;
+    }
+});
+
 
 
 
